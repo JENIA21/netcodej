2a82db5a97ee07395f4a8f497a678f3c7000d7c8
JENIA21 <jenia8513@gmail.com>
2023-02-09 18:02:42 +0500

    fix

--- src/main/java/netcodej/Main.java
+++ src/main/java/netcodej/Main.java
@@ -2,18 +2,14 @@ package netcodej;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 1;
-        int b = 1;
-        int c = 2;
         var bw = new BitWriter();
-        bw.writeValueIfChanged(a, b, new LimitInt(0, 8));
-        bw.writeValueIfChanged(a, c, new LimitInt(0, 8));
+        bw.write(5, 0, 32);
+        bw.write(4, 0, 32);
+        System.out.println(bw.getBitsCount());
         bw.flush();
+        System.out.println(bw.getBitsCount());
         var br = new BitReader(bw.getBytes());
-        var ab = br.readIntIfChanged(0, 8);
-        var ac = br.readIntIfChanged(0, 8);
-        System.out.println(ab.isError);
-        System.out.println(ac.isError);
-        System.out.println(ac.value);
+        System.out.println(br.readInt(0, 32));
+        System.out.println(br.readInt(0, 32));
     }
 }
